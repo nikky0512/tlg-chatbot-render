9b96021efd87c545de7fa7016e04606bacf15e1b
nikky0512 <nikolaniksapavletic@gmail.com>
2025-04-23 16:36:23 +0200

    Update handlers.py
    asd

--- src/handlers/handlers.py
+++ src/handlers/handlers.py
@@ -8,9 +8,9 @@ from telethon.tl.custom import Button
 from telethon.tl.functions.messages import SetTypingRequest
 from telethon.tl.types import SendMessageTypingAction
 
-import src.utils.utils
-from src.functions.additional_func import bash, search
-from src.functions.chat_func import (
+import utils.utils
+from functions.additional_func import bash, search
+from functions.chat_func import (
     get_bard_response,
     get_bing_response,
     get_gemini_response,
@@ -19,7 +19,7 @@ from src.functions.chat_func import (
     process_and_send_mess,
     start_and_check,
 )
-from src.utils import (
+from utils import (
     ALLOW_USERS,
     LOG_PATH,
     MODEL_DICT,
@@ -84,25 +84,18 @@ async def clear_handler(event: NewMessage) -> None:
 
 @register(NewMessage(pattern="/bard"))
 async def bard_chat_handler(event: NewMessage) -> None:
-    # Get info
     chat_type, client, chat_id, message = await check_chat_type(event)
     if chat_type == "User":
         message = message.split(" ", maxsplit=1)[1]
     logging.debug(f"Check chat type {chat_type} done")
     await client(SetTypingRequest(peer=chat_id, action=SendMessageTypingAction()))
-
-    # Inialize
     loop = asyncio.get_event_loop()
-
-    # Get response from openAI
     future = loop.run_in_executor(None, get_bard_response, message)
-    while not future.done():  # Loop of random actions indicates running process
+    while not future.done():
         random_choice = random.choice(RANDOM_ACTION)
         await asyncio.sleep(2)
         await client(SetTypingRequest(peer=chat_id, action=random_choice))
     response = await future
-
-    # Send response to chat id
     try:
         await process_and_send_mess(event, response)
         logging.debug(f"Sent message to {chat_id}")
@@ -115,82 +108,40 @@ async def bard_chat_handler(event: NewMessage) -> None:
 
 @register(NewMessage(pattern="/bing"))
 async def bing_chat_handler(event: NewMessage) -> None:
-    # Get info
     chat_type, client, chat_id, message = await check_chat_type(event)
     if chat_type == "User":
         message = message.split(" ", maxsplit=1)[1]
     logging.debug(f"Check chat type {chat_type} done")
     await client(SetTypingRequest(peer=chat_id, action=SendMessageTypingAction()))
-
-    # Under maintainance
     await event.reply("**Cannot use this anymore**")
-    raise StopPropagation
-
-    # Inialize
-    loop = asyncio.get_event_loop()
-
-    # Get response from openAI
-    future = loop.run_in_executor(None, get_bing_response, message)
-    while not future.done():  # Loop of random actions indicates running process
-        random_choice = random.choice(RANDOM_ACTION)
-        await asyncio.sleep(2)
-        await client(SetTypingRequest(peer=chat_id, action=random_choice))
-    response, suggest_lst = await future
-
-    buttons = [
-        [Button.text(text=f"/bing {text}", single_use=True)] for text in suggest_lst
-    ]
-    buttons.append([Button.text(text=f"/cancel", single_use=True)])
-    # Send response to chat id
-    try:
-        await event.client.send_message(
-            event.chat_id,
-            response,
-            background=True,
-            silent=True,
-            buttons=buttons,
-        )
-        logging.debug(f"Sent message to {chat_id}")
-    except Exception as e:
-        logging.error(f"Error occurred when handling {chat_type} chat: {e}")
-        await event.reply("**Fail to get response**")
-    await client.action(chat_id, "cancel")
     raise StopPropagation
 
 
 @register(NewMessage(pattern="/gemini"))
 async def gemini_chat_handler(event: NewMessage) -> None:
-    # Get info
     chat_type, client, chat_id, message = await check_chat_type(event)
     if chat_type == "User":
         message = message.split(" ", maxsplit=1)[1]
     logging.debug(f"Check chat type {chat_type} done")
     await client(SetTypingRequest(peer=chat_id, action=SendMessageTypingAction()))
-
     try:
         file_name = await event.download_media("temp")
-    except:
+    except Exception as e:
         logging.error(f"Error occurred when downloading media: {e}")
         await event.reply("**Error occurred when downloading media**")
         raise StopPropagation
-
-    # Inialize
     loop = asyncio.get_event_loop()
-
-    # Get response from openAI
     if not file_name:
         future = loop.run_in_executor(None, get_gemini_response, message)
     else:
         future = loop.run_in_executor(
             None, get_gemini_vison_response, message, file_name
         )
-    while not future.done():  # Loop of random actions indicates running process
+    while not future.done():
         random_choice = random.choice(RANDOM_ACTION)
         await asyncio.sleep(2)
         await client(SetTypingRequest(peer=chat_id, action=random_choice))
     response = await future
-
-    # Send response to chat id
     try:
         await process_and_send_mess(event, response)
         logging.debug(f"Sent message to {chat_id}")
@@ -212,20 +163,13 @@ async def switch_model_handler(event: NewMessage) -> None:
                 event.chat_id,
                 f"Model not found, available models: **{available_models}**",
             )
-        elif MODEL_DICT[model][0] == src.utils.utils.model:
+        elif MODEL_DICT[model][0] == utils.utils.model:
             await client.send_message(
                 event.chat_id, f"**{MODEL_DICT[model][0]}** is being used already"
             )
         else:
-            src.utils.utils.model = MODEL_DICT[model][0]  # Overwrite system MODEL
-            src.utils.utils.max_token = MODEL_DICT[model][1]
-            # TODO: This is wrong but save it for future switchtone
-            # if len(glob.glob(f"{LOG_PATH}chats/history/{event.chat_id}*")) > 0:
-            #     await client.send_message(
-            #         event.chat_id,
-            #         f"Successfully set model to **{MODEL_DICT[model][0]}**\n\nSwitching model requires a chat history reset, please perform /clear and the change will be applied.",
-            #     )
-            # else:
+            utils.utils.model = MODEL_DICT[model][0]
+            utils.utils.max_token = MODEL_DICT[model][1]
             await client.send_message(
                 event.chat_id,
                 f"Successfully set model to **{MODEL_DICT[model][0]}**",
@@ -238,27 +182,20 @@ async def switch_model_handler(event: NewMessage) -> None:
 
 @register(NewMessage(pattern="/senpai"))
 async def senpai_chat_handler(event: NewMessage) -> None:
-    # Get info
     chat_type, client, chat_id, message = await check_chat_type(event)
     if chat_type == "User":
         message = message.split(" ", maxsplit=1)[1]
     logging.debug(f"Check chat type {chat_type} done")
     await client(SetTypingRequest(peer=chat_id, action=SendMessageTypingAction()))
-    src.utils.utils.sys_mess = SYS_MESS_SENPAI  # Overwrite system mess
-
-    # Inialize
+    utils.utils.sys_mess = SYS_MESS_SENPAI
     filename, prompt = await start_and_check(event, message, chat_id)
     loop = asyncio.get_event_loop()
-
-    # Get response from openAI
     future = loop.run_in_executor(None, get_openai_response, prompt, filename)
-    while not future.done():  # Loop of random actions indicates running process
+    while not future.done():
         random_choice = random.choice(RANDOM_ACTION)
         await asyncio.sleep(2)
         await client(SetTypingRequest(peer=chat_id, action=random_choice))
     response = await future
-
-    # Send response to chat id
     try:
         await process_and_send_mess(event, response)
         logging.debug(f"Sent message to {chat_id}")
@@ -271,28 +208,21 @@ async def senpai_chat_handler(event: NewMessage) -> None:
 
 @register(NewMessage)
 async def user_chat_handler(event: NewMessage) -> None:
-    # Get info
     chat_type, client, chat_id, message = await check_chat_type(event)
-    if chat_type != "User":  # Prevent no command group chat
+    if chat_type != "User":
         return
     else:
         logging.debug(f"Check chat type {chat_type} done")
     await client(SetTypingRequest(peer=chat_id, action=SendMessageTypingAction()))
-    src.utils.utils.sys_mess = SYS_MESS_SENPAI  # Overwrite system mess
-
-    # Inialize
+    utils.utils.sys_mess = SYS_MESS_SENPAI
     filename, prompt = await start_and_check(event, message, chat_id)
     loop = asyncio.get_event_loop()
-
-    # Get response from openAI
     future = loop.run_in_executor(None, get_openai_response, prompt, filename)
-    while not future.done():  # Loop of random actions indicates running process
+    while not future.done():
         random_choice = random.choice(RANDOM_ACTION)
         await asyncio.sleep(2)
         await client(SetTypingRequest(peer=chat_id, action=random_choice))
     response = await future
-
-    # Send response to chat id
     try:
         await process_and_send_mess(event, response)
         logging.debug(f"Sent message to {chat_id}")
@@ -305,28 +235,21 @@ async def user_chat_handler(event: NewMessage) -> None:
 
 @register(NewMessage(pattern="/slave"))
 async def group_chat_handler(event: NewMessage) -> None:
-    # Get info
     chat_type, client, chat_id, message = await check_chat_type(event)
     if chat_type != "Group":
         return
     else:
         logging.debug(f"Check chat type {chat_type} done")
     await client(SetTypingRequest(peer=chat_id, action=SendMessageTypingAction()))
-    src.utils.utils.sys_mess = SYS_MESS_FRIENDLY  # Overwrite system mess
-
-    # Inialize
+    utils.utils.sys_mess = SYS_MESS_FRIENDLY
     filename, prompt = await start_and_check(event, message, chat_id)
     loop = asyncio.get_event_loop()
-
-    # Get response from openAI
     future = loop.run_in_executor(None, get_openai_response, prompt, filename)
-    while not future.done():  # Loop of random actions indicates running process
+    while not future.done():
         random_choice = random.choice(RANDOM_ACTION)
         await asyncio.sleep(2)
         await client(SetTypingRequest(peer=chat_id, action=random_choice))
     response = await future
-
-    # Send response to chat id
     try:
         await process_and_send_mess(event, response)
         logging.debug(f"Sent message to {chat_id}")
